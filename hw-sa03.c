3c74f9d2fb7746c98b5b4e6d04c82d732faa7bab
Asano-H <princessknk0406@gmail.com>
2017-09-17 23:39:39 +0900

    commit

---
+++ hw-sa03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+	double x,y,z;
+
+	x=2.0;
+	y=3.0;
+	z=pow(x,y);
+
+	printf("x=%f\n",x);
+	printf("y=%f\n",y);
+	printf("z=%f\n",z);
+	return 0;
+	
+}
