9c6c8a3d621c3743dc729ef531da3ec0f9847285
Asano-H <princessknk0406@gmail.com>
2017-09-29 22:31:55 +0900

    commit

--- ts-17-0929-1.c
+++ ts-17-0929-1.c
@@ -2,7 +2,7 @@
 void fill(int a[], int n, int v){
 	int i;
 	for(i=0; i<n; i++){
-		a[i]=v;
+		a[i]=v;	/*配列aにv(=0)を代入*/
 	}
 
 void fill0(int a[], int n){
@@ -10,11 +10,11 @@ void fill0(int a[], int n){
 }
 
 int main(void){
-	int a[8]={1,1,1,1, 1,1,1,1};
+	int a[8]={1,1,1,1, 1,1,1,1};	/*配列aを1で初期化*/
 	int j;
 	fill0(a,4);
 	for(j=0; j<8; j++){
-		printf("%d",a[j]);
+		printf("%d",a[j]);	/*配列の要素を出力*/
 	}
 	return 0;
 }
