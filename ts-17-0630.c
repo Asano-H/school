f40b69033d196e22eb131a9afa28b13371b86a38
Asano-H <princessknk0406@gmail.com>
2017-10-02 17:20:22 +0900

    commit

--- ts-17-0630.c
+++ ts-17-0630.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 void suretu_keisan(int n){
 	int i,j;
-	int a[101]=[1,1];
+	int a[101]={1,1};
 	for(i=2;i<=n;i++)
 		a[i]=a[i-2] + a[i-1];
 	for(j=0;j<=n-1;j++)
 		printf("%d\n",a[j]);	
 }
-
 int main(void){
 	int x;
 	printf("何項まで表示しますか(1～100)：");
