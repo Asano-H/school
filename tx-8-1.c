15eb66917d6e4c7789f200b7e62c8c863496c5a3
Asano-H <princessknk0406@gmail.com>
2017-10-05 17:06:11 +0900

    commit

---
+++ tx-8-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define diff(x,y) ((x > y) ? (x - y) : (y - x)) 
+
+int main(void){
+	int a,b;
+	printf("“ñ‚Â‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf("%d,%d",&a,&b);
+	printf("“ñ‚Â‚Ì®”‚Ì·‚Í%d‚Å‚·",diff(a,b));
+	return 0;
+}
