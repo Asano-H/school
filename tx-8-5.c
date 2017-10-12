f9b09468d832e182db2b0b559ac51fddedcf28ac
Asano-H <princessknk0406@gmail.com>
2017-10-12 17:27:46 +0900

    commit

---
+++ tx-8-5.c
@@ -0,0 +1,25 @@
+/*バブルソート*/
+#include<stdio.h>
+#define NUMBER 5
+
+void sort(int a[],int n){
+	int i,j;
+	for(i=n; i>0; i--){
+		if(a[j-1]>a[j]){
+			int temp = a[j];
+			a[j]     = a[j-1];
+			a[j-1]   = temp;
+		}
+	}
+}
+
+int main(void){
+	int weight[NUMBER];
+	int i;
+	printf("%d人の体重を入力してください",NUMBER);
+	for(i=0; i<NUMBER; i++){
+		printf("%d番",i+1);
+		scanf("%d",weight[i]);
+	}
+
+}
