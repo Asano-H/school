eed93c05aa90843dd779850aa886997855020a0c
Asano-H <princessknk0406@gmail.com>
2017-10-19 20:59:37 +0900

    fix

--- tx-8-9.c
+++ tx-8-9.c
@@ -5,11 +5,11 @@ int comb(int n,int r){
 		return 1;
 	else if( r==1 )
 		return n;
-	return conb(n-1,r-1) + comb(n-1,r);
+	return comb(n-1,r-1) + comb(n-1,r);
 }
 int main(void){
 	int n,r;
-	printf("異なるn個からr個を取り出す組み合わせの数を求める");
+	printf("異なるn個からr個を取り出す組み合わせの数を求める\n");
 	printf("n：");	scanf("%d",&n);
 	printf("r：");	scanf("%d",&r);
 	printf("組み合わせの数：%d",comb(n,r));
