83f793860f70fc7a0cab9095ffc0367df1609962
Asano-H <princessknk0406@gmail.com>
2017-10-19 21:22:30 +0900

    fix

--- tx-8-10.c
+++ tx-8-10.c
@@ -1,24 +1,24 @@
-/*最大公約数*/
+/*�ő���񐔂����߂�*/
+#include<stdio.h>
 int gcd(int x,int y){
 	if(x==y)
 		return x;
 	else 
-		return gcd(y,x&y);
-	}
+		return gcd(y,x%y);
 }
+
 int main(void){
 	int x,y;
-	printf("最大公約数を求めます\n");
-	printf("Ｘ：");	scanf("%d",&x);
-	printf("Ｙ：");	scanf("%d",&y);
+	printf("�ő���񐔂����߂܂�");
+	printf("�w�F");	scanf("%d",&x);
+	printf("�x�F");	scanf("%d",&y);
 
-	printf("最大公約数は%dです",gcd(x,y));
+	printf("�ő���񐔂�%d�ł�",gcd(x,y));
 	return 0;
 }
-
-
-/*ユークリッドの互除法
-　２つの整数値を二辺とした長方形を作るとする。
-　短い方の値を１辺とする正方形を作っていき、
-　余った長方形もさらに短い１辺で正方形を作っていく
-　余りが出ないきれいな正方形ができたとき、その１辺の値が最大公約数*/
+/*���[�N���b�h�̌ݏ��@
+�Q�̐������Q�ӂƂ��������`�����B
+�Z���ق��̕ӂ��P�ӂƂ��������`������Ă����B
+�]���������`���A����ɒZ���ӂŐ����`�����B
+�]�肪�o���Y��ɐ����Ŗ��ߐs�����ꂽ�Ƃ��A
+�ł������������`�̂P�ӂ��ő���񐔂ƂȂ�B*/
