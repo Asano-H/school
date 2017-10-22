0535c8618eaf45db013ecfcb44f05bd36e804de1
Asano-H <princessknk0406@gmail.com>
2017-10-22 17:32:44 +0900

    commit

---
+++ ts-17-1020-2a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int fact(int n){
+	if(n>0)
+		return 1;
+	else
+		return n*fact(n-1);
+}
+
+int main(void){
+	int num;
+	printf("整数を入力してください：");
+	scanf("%d",&num);
+	printf("%dの階乗は%dです",num,fact(n));
+
+	return 0;
+}
