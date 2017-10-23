0a0591dc67ffc67f0ffc8387c8e67b060a86ba4b
Asano-H <princessknk0406@gmail.com>
2017-10-23 20:39:15 +0900

    fix

--- tx-8-12.c
+++ tx-8-12.c
@@ -3,15 +3,15 @@ int main(void){
 	int i,j,r;
 	int cnt[10]={0};
 
-	while((r=getchar() != EOF)){
+	while((r=getchar()) != EOF){
 		if(r >= '0' && r <= '9')
 			cnt[r-'0']++;
 	}
 	printf("”Žš•¶Žš‚ÌoŒ»‰ñ”");
-	for(i=0;9<10;i++){
-		printf("%d",i);
+	for(i=0;i<10;i++){
+		printf("%dF",i);
 		for(j=0;j<cnt[i];j++)
-			printf("*")
+			printf("–");
 		printf("\n");
 	}
 	return 0;
