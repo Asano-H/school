cf332468fe932f7a931bed1fab0c1cd458536753
Asano-H <princessknk0406@gmail.com>
2017-10-24 23:56:58 +0900

    add

--- tx-8-13.c
+++ tx-8-13.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(void){
+	
 	printf("EOFの値は\"%d\"です。\n",EOF);
 	printf("EOFの値は%dです。",EOF);
 	putchar('"');	/*「”」と表示*/
 	putchar('\"');	/*「”」と表示*/
 	putchar('\'');	/*「'」と表示*/
+	puts("'");		/*「'」と表示*/
+
 	return 0;
 }
