5f12982f18482e7288aa128193cec44c24313bda
Asano-H <princessknk0406@gmail.com>
2017-10-26 20:19:23 +0900

    fix

--- txm-8-56.c
+++ txm-8-56.c
@@ -2,9 +2,11 @@
 int main(void){
 	int no;
 	int i=0;
-	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
 	scanf("%d",&no);
-
 	for(i=0; i<no; i++)
-		putchar('0'+no%3+1);
+		putchar('0'+i%3+1);
+
+	return 0;
 }
