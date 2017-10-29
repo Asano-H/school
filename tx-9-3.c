70ffb6535548e3bc94b4cfad09d9e6e6c5be23cf
Asano-H <princessknk0406@gmail.com>
2017-10-29 20:39:50 +0900

    commit

--- tx-9-3.c
+++ tx-9-3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(void){
-	int s[],str[];
-	s[] = "ABC";
+	char s[100],str[100];
+	s = "ABC";
 	printf("str‚ÍF");
 	scanf("%s",&str);
 
 	printf("s‚ÍF%s",s);
 	printf("str‚ÍF%s",str);
 
-	s = '\0';
-	str = '\0';
+	s[0] = '\0';
+	str[0] = '\0';
 	printf("s‚Í%s",s);
 	printf("str‚Í%s",str);
 
