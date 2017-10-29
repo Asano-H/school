7921df48c4f6ed6eab3a794ac50f41eeb6263980
Asano-H <princessknk0406@gmail.com>
2017-10-29 20:36:32 +0900

    commit

---
+++ tx-9-3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void){
+	int s[],str[];
+	s[] = "ABC";
+	printf("str‚ÍF");
+	scanf("%s",&str);
+
+	printf("s‚ÍF%s",s);
+	printf("str‚ÍF%s",str);
+
+	s = '\0';
+	str = '\0';
+	printf("s‚Í%s",s);
+	printf("str‚Í%s",str);
+
+	return 0;
+
+}
