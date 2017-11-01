4e417111e114f340ae5ba22cfd991cc89e73a88a
Asano-H <princessknk0406@gmail.com>
2017-11-01 18:00:50 +0900

    add

--- typing.c
+++ typing.c
@@ -2,14 +2,20 @@
 #include<time.h>
 #include<stdlib.h>
 
+int ransu(void){
+	int a;
+	srand((unsigned)time(NULL));
+	a = rand()%3+1;
+	return a;
+}
+
 void typing_color(void){
 	char word[4][50]={"red","blue","green","yellow"};
 	int i;
-	srand((unsigned)time(NULL));
-	for(i=0;i<5;i++)
-		printf("%d",rand()%4+1);
+	for(i=0;i<5;i++){
+		printf("%s\n",word[ransu()]);
 
-	return 0;
+	}
 }
 
 int main(void){
