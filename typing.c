254ba86bcf414a6d5903f5812eac9b977027b23c
Asano-H <princessknk0406@gmail.com>
2017-11-01 17:44:37 +0900

    add

---
+++ typing.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void typing_color(void){
+	char word[]={"red","blue","green","yellow"};
+	int i;
+	srand((unsigned)time(NULL));
+	for(i=0;i<5;i++)
+		printf("%d",rand()%4+1);
+
+	return 0;
+}
+
+int main(void){
+	typing_color();
+	return 0;
+}
