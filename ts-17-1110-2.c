a54ce1d2f5568f1830cc3686c4a45f29323be2f9
Asano-H <princessknk0406@gmail.com>
2017-11-10 19:54:16 +0900

    commit

---
+++ ts-17-1110-2.c
@@ -0,0 +1,15 @@
+/*文字列"0123456789"を逆順に表示するプログラム*/
+#include<stdio.h>
+int main(void){
+	char str[]="01234568789";
+	char ch;
+	int i;
+
+	for(i=0;i<=9/2;i++){
+		ch = str[i];
+		str = str[9-i];
+		str[9-i] = ch;		
+	}
+	str[10]='\0';
+	return 0;
+}
