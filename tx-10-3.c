4946a9303614055777735e4186c9ea47ba07d19d
Asano-H <princessknk0406@gmail.com>
2017-11-18 18:12:20 +0900

    fix

--- tx-10-3.c
+++ tx-10-3.c
@@ -10,25 +10,24 @@ void swap(int *a1, int *a2){
 void sort3(int *n1, int *n2, int *n3){
 	if(*n1>*n2)	swap(n1,n2);
 	if(*n2>*n3)	swap(n2,n3);
-	if(*n3>*n1)	swap(n3,n1); 
-
+	if(*n1>*n2)	swap(n1,n2);
 }
 
 int main(void){
 	int no1,no2,no3;
 
-	printf("三つの整数を入力してください");
-	printf("整数1：");
+	printf("三つの整数を入力してください\n");
+	printf("整数1:");
 	scanf("%d",&no1);
-	printf("整数2：");
+	printf("整数2:");
 	scanf("%d",&no2);
-	printf("整数3：");
+	printf("整数3:");
 	scanf("%d",&no3);
 
 	sort3(&no1,&no2,&no3);
 
-	puts("昇順に入れ替えました");
-	printf("整数1は%d",no1);
-	printf("整数2は%d",no2);
-	printf("整数3は%d",no3);
+	puts("昇順にソートしました");
+	printf("整数1:%d\n",no1);
+	printf("整数2:%d\n",no2);
+	printf("整数3:%d\n",no3);
 }
