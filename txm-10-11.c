75c5b8c89c1f705a4f5e0a780adb8d2bb61e96d7
Asano-H <princessknk0406@gmail.com>
2017-11-19 12:34:45 +0900

    add

--- txm-10-11.c
+++ txm-10-11.c
@@ -6,5 +6,15 @@ void div(int a,int b,int *quot,int *rem){
 }
 
 int main(void){
+	int a,b;
+	int ansqu,ansre;
 
+	printf("２つの整数を入力してください\n");
+	printf("整数A:");
+	scanf("%d",&a);
+	printf("整数B:");
+	scanf("%d",&b);
+
+	div(a,b,ansqu,ansre);
+	printf("%d÷%d=%dあまり%d",a,b,ansqu,ansre);
 }
