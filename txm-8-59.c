f65837f0b3f8a97db10e99dd2fb741d183b419cd
Asano-H <princessknk0406@gmail.com>
2017-11-20 17:40:02 +0900

    add

--- txm-8-59.c
+++ txm-8-59.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main(void){
-	enum singou{red,yellow,blue}
+	enum singou{red,yellow,blue};
 	int sign;
 
 	printf("数値を入力してください(0~2)");
 	scanf("%d",sign);
 
-	case(sign){
-		red:	printf("とまれ");		break;
-		yellow:	printf("ちゅうい");	break;
-		blue:	printf("すすめ");		break;
+	swith(sign){
+		case red:	printf("とまれ");		break;
+		case yellow:	printf("ちゅうい");	break;
+		case blue:	printf("すすめ");		break;
 	}
 	return 0;
-
 }
