7ed9a7f76b43b90c99b51ef12e939ed9d938b144
Asano-H <princessknk0406@gmail.com>
2017-11-21 19:46:53 +0900

    fix

--- tx-10-4.c
+++ tx-10-4.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void set(int *a, int no){
+void set(int a, int no){
 	int i;
 	for(i=0;i<n;i++){
 		v[i]=i;
@@ -8,8 +8,8 @@ void set(int *a, int no){
 }
 int main(void){
 	int i;
-	int *a[10];
-	set(&a,5);
+	int a[10];
+	set(a,5);
 
 	for(i=0;i<10;i++){
 		printf("a[%d]=%d",i,a[i]);
