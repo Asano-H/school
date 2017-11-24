911d0acc9006e2db57890ee7dc204d3ed4b8a991
Asano-H <princessknk0406@gmail.com>
2017-11-24 14:08:37 +0900

    add

--- txm-9-37.c
+++ txm-9-37.c
@@ -1,10 +1,16 @@
-/**/
+/*文字列の中から数字だけをとりだす*/
 #include<stdio.h>
 
-void del_non(char){
+void del_non(char s[]){
+	int i
 
 }
 
 int main(void){
+	char str[];
+	printf("文字を入力してください\n");
+	scanf("%s",str);
+
+	del_non(str);
 
 }
