25f64004eccf88df9290b3f6af88675711b2a475
Asano-H <princessknk0406@gmail.com>
2017-11-24 14:15:44 +0900

    add

--- txm-9-37.c
+++ txm-9-37.c
@@ -5,17 +5,19 @@ void del_non(char s[]){
 	int i;
 	int x;
 	while(s[i]){
-		if(s[i] >= '0' && s[i]<='9'){
+		if(s[i] >= '0' && s[i]<='9')
 			s[x++] = s[i];
 		i++;
 	}
+	s[x] = '\0';
 }
 
 int main(void){
-	char str[];
+	char str[500];
 	printf("文字を入力してください\n");
 	scanf("%s",str);
 
 	del_non(str);
+	printf("%s",str);
 
 }
