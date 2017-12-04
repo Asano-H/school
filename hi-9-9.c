d9e83d09786639232866a9091935adf21fce87e2
Asano-H <princessknk0406@gmail.com>
2017-12-04 23:19:56 +0900

    commit

---
+++ hi-9-9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void){
+	char str[256];
+	printf("•¶Žš—ñ‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢:");
+	scanf("%s",str);
+
+	printf("%d",strlen(str));
+
+	return 0;
+}
