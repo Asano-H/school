eaf17546c17ade0071ceaa7eacbfd2841fe31ee0
Asano-H <princessknk0406@gmail.com>
2018-01-16 23:27:01 +0900

    commit

---
+++ hi-11-5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swapchar(char *s, int d1, int d2){
+	char temp;
+	temp = s[d1];
+	s[d1] = s[d2];
+	s[d2] = temp;
+
+}
+
+int main(void){
+	char s[]= "hamster";
+	puts(s);
+	swapchar(s,2,5);
+	puts(s);
+	return 0;
+
+}
