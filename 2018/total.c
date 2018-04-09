2135601f5ce0bb6a3b3013c3f10d5b96a6f64d0c
Asano-H <princessknk0406@gmail.com>
2018-04-09 19:57:18 +0900

    commit

---
+++ 2018/total.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+        int sum,no;
+        printf("no: ");
+        scanf("%d",&no);
+        sum = no*(no+1)/2;
+        printf("total= %d\n",sum);
+
+        return 0;
+}
