7211bcf7b3ecdd90071a32b8236525543a03d55f
Asano-H <princessknk0406@gmail.com>
2018-04-09 19:56:57 +0900

    commit

---
+++ 2018/dial.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+        int i,num;
+        for(i=0;i<7;i++){
+                num=142857*i;
+                printf("142857×%d = %d\n",i,num);
+        }
+        return 0;
