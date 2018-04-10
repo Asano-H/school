ddd57b0d3bb1e75a5d6f4cf5f2ca030152912ccb
Asano-H <princessknk0406@gmail.com>
2018-04-10 18:40:58 +0900

    fix code

--- 2018/dial.c
+++ 2018/dial.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(void){
         int i,num;
-        for(i=0;i<7;i++){
+        for(i=1;i<8;i++){
                 num=142857*i;
-                printf("142857×%d = %d\n",i,num);
+                printf("142857 × %d = %d\n",i,num);
         }
         return 0;
+}
