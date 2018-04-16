8d481acf40ce68f34fe9302366777019ce33c01c
Asano-H <princessknk0406@gmail.com>
2018-04-16 22:29:34 +0900

    commit

---
+++ 2018/atox.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	int a;
+	long b;
+	double c;
+	float d;
+
+	a = atoi("123");
+	b = atof("3.65");
+
+	printf("%d\n",a);
+	printf("%lf",b);
+
+	return 0;
+}
