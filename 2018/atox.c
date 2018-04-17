337a1dd3c57145aee0e88f1d0c5a463edf46c286
Asano-H <princessknk0406@gmail.com>
2018-04-17 21:24:41 +0900

    add code

--- 2018/atox.c
+++ 2018/atox.c
@@ -7,10 +7,38 @@ int main(void){
 	float d;
 
 	a = atoi("123");
-	b = atof("3.65");
+	b = atol("123");
+	// c = atoff("1.23");
+	d = atof("1.234567");
 
-	printf("%d\n",a);
-	printf("%lf",b);
+	printf("a= %d\n",a);
+	printf("b= %ld\n",b);
+	// printf("c= %f\n",c);
+	printf("d= %lf\n",d);
+	printf("\n");
+
+	a = atoi("1X23");
+	b = atol("12X3");
+	// c = atoff("1.2X3");
+	d = atof("1X.23456");
+
+	printf("a= %d\n",a);
+	printf("b= %ld\n",b);
+	// printf("c= %f\n",c);
+	printf("d= %lf\n",d);
+	printf("\n");
+
+	a = atoi("X12");
+	b = atol("X12");
+	// c = atoff("X1.2");
+	d = atof("X1.23456")
+
+	printf("a= %d\n",a);
+	printf("b= %ld\n",b);
+	// printf("c= %f\n",c);
+	printf("d= %lf\n",d);
+	printf("\n");
+
 
 	return 0;
 }
