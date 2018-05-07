f641770a393c3b2f30441a84cac1ff7a51310e2f
Asano-H <princessknk0406@gmail.com>
2018-05-07 13:44:02 +0900

    commit

---
+++ 2018/oneup.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int oneup(int n,int g){
+	int up=0;
+
+	if((n<10000)&& (n+g>=10000)){
+		g -= 10000-n;
+		n = 0;
+		up = 1;
+
+	}else{
+		n =10000; 
+	}
+
+	up += ((n+g)/30000)-(n/30000);
+
+	return up;
+}
+
+
+int main(void){
+	int now_p,gets_p;
+
+	printf("現在のポイント: ");
+	scanf("%d",&now_p);
+	printf("何ポイントの敵？: ");
+	scanf("%d",&gets_p);
+
+	printf("%dup",oneup(now_p,gets_p));
+
+	return 0;
+
+}
