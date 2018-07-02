53bd63ca959adadccf0b95bb70e97b451ceea036
Asano-H <princessknk0406@gmail.com>
2018-07-02 14:13:47 +0900

    fix code

--- 2018/ファイル処理/maxminave.c
+++ 2018/ファイル処理/maxminave.c
@@ -1,22 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 #define N 100	//ファイル内のデータ数
 
 int main(void){
 	FILE *fp;
 	fp=fopen("./max_min_ave.txt","r");	//ファイルを読み込む
 
-	int ave=0, max=0, min=1000,sort[N];
+	int ave=0, max=0, min=RAND_MAX,sort[N];
 
 //ファイル読み込み
 	for(int i=0;i<N;i++){
 		fscanf(fp,"%d\n",&sort[i]);	//ファイルから一行ずつ読み込み
 	}
 	fclose(fp);
-
-//平均値を求める
-	for(int i=0; i<N; i++){
-		ave+=sort[i];
-	}
 
 //最大値を求める
 	for(int i=0; i<N; i++){
@@ -32,8 +29,13 @@ int main(void){
 		}
 	}
 
-	printf("ave: %d\n",ave/N);
+//平均値を求める
+	for(int i=0; i<N; i++){
+		ave+=sort[i];
+	}
+
 	printf("max: %d\n",max);
 	printf("min: %d\n",min);
+	printf("ave: %d\n",ave/N);
 
 }
